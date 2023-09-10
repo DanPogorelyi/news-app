8c473161a33adf3e9c4a2b4535764bc1dc96c249
Pogorelyi Daniil <--value>
2023-09-10 07:39:19 +0400

    delete env var

--- src/features/AuthByUsername/model/slice/loginSlice.spec.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.spec.ts
@@ -1,21 +1,23 @@
-import { LoginSchema } from '../types/loginSchema';
+// import { LoginSchema } from '../types/LoginSchema';
 import { loginActions, loginReducer } from './loginSlice';
 
+// FIXME any
 describe('loginSlice', () => {
     it('test set username', () => {
-        const state: DeepPartial<LoginSchema> = { userName: '123' };
+        // const state: DeepPartial<LoginSchema> = { userName: '123' };
+        const state: DeepPartial<any> = { userName: '123' };
 
         expect(loginReducer(
-            state as LoginSchema,
+            state as any,
             loginActions.setUserName('123123'),
         )).toEqual({ userName: '123123' });
     });
 
     it('test set password', () => {
-        const state: DeepPartial<LoginSchema> = { password: '123' };
+        const state: DeepPartial<any> = { password: '123' };
 
         expect(loginReducer(
-            state as LoginSchema,
+            state as any,
             loginActions.setPassword('123123'),
         )).toEqual({ password: '123123' });
     });
